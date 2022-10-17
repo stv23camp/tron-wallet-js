70979a681a501ae10b387e21112ce7df4579a789
steve23tkn <stephen@tokenomy.com>
2022-10-17 07:00:02 +0700

    cron: add trx deposit scan worker

---
+++ src/services/deposit_trx.js
@@ -0,0 +1,12 @@
+const cron = require('node-cron');
+require('dotenv').config();
+const sentry = require('./library/sentry_obj');
+const deposit = require('./src/controller/deposit');
+
+cron.schedule("*/2 * * * *", async function(){
+    try {
+        await deposit.scanTrx();
+    } catch(e) {
+        sentry.captureException(e.message);
+    }
+});
