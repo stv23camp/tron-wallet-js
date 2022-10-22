09a9b053aa71fe703251c276505a8b82a4a7a1a6
steve23tkn <stephen@tokenomy.com>
2022-10-22 11:11:22 +0700

    controller/optimize forwarding async processes

--- src/controllers/forward.js
+++ src/controllers/forward.js
@@ -3,45 +3,83 @@ const tron = require('../library/tron');
 const db = require('../library/db_mysql');
 const encryption = require('../library/encryption');
 
+function delay(ms){
+    return new Promise(async (resolve)=>{
+        setTimeout(()=>resolve(), ms);
+    });
+}
+
+function getNative(addr){
+    return new Promise(async (resolve)=>{
+        const result = await tron.getBalanceNative(addr);
+        resolve(result);
+    });
+}
+
+function getUsdt(addr, contract, digit){
+    return new Promise(async (resolve)=>{
+        const result = await tron.getBalanceTrc20(addr, contract, digit);
+        resolve(result);
+    });
+}
+
+function getPk(addr){
+    return new Promise( async (resolve) => {
+        const encrypted = await db.getPrivate(addr);
+        resolve(encrypted);
+    });
+}
+
+function sendTrx(addr, poolAddr, balance_raw, decrypted) {
+    return new Promise(async (resolve) => {
+        const txid = await tron.sendNative(addr, poolAddr, balance_raw, decrypted);
+        resolve(txid);
+    });
+}
+
+function sendUsdt(addr, poolAddr, balance_raw, contract, decrypted){
+    return new Promise(async (resolve) => {
+        const txid = await tron.sendToken(addr, poolAddr, balance_raw, contract, decrypted);
+        resolve(txid);
+    });
+}
+
 async function sendTrxToPool(){
     const poolAddr = process.env.POOL;
-
-    // get 100 recent TRX incoming txs from DB
     const txs = await db.getTransactionsNative();
     const receivers = txs.map((item)=>{
         return item.to;
     });
-    // reduce txs into distinct addresses
-    const distinct_addresses = [...new Set(receivers)];
+
+    console.log('receivers: ', receivers.length);
+
+    const receivers_filt = receivers.filter((item)=>{
+        if (item!=poolAddr) return true;
+    });
+    console.log('poolAddr removed: ', receivers_filt.length);
     
-    // iterate addresses
+    const distinct_addresses = [...new Set(receivers_filt)];
+    console.log('unique: ', distinct_addresses.length);
+
     for (let addr of distinct_addresses) {
-        // getbalance for this address
-        const balance = await tron.getBalanceNative(addr);
+        const balance = await getNative(addr);
+        await delay(1000);
+        console.log(balance);
         const balance_raw = parseFloat(balance) * 10**6;
-        // if balance <=10 skip 
         if (balance<=10) continue;
-
-        // forward to pool
-        const encrypted = await db.getPrivate(addr);
-
+        console.log(addr);
+        const encrypted = await getPk(addr);
         if (!encrypted) {
             console.log('pk not found');
             continue;
         }
-
         const decrypted = encryption.decryptKey(encrypted);
-
         console.log(`sending ${balance} trx from ${addr}`)
-        const txid = await tron.sendNative(addr, poolAddr, balance_raw, decrypted);
-
-        // if sending failed inform admin via throw
+        const txid = await sendTrx(addr, poolAddr, balance_raw, decrypted);
         if (!txid || txid.length!=64) {
             throw new Error('Forward trx failed');
             continue;
         }
-
-        // console log success
         console.log('Success tx: ', txid);
     }
     console.log('forwarding session ended');
@@ -50,58 +88,62 @@ async function sendTrxToPool(){
 async function sendTokenToPool(token){
     const poolAddr = process.env.POOL;
 
-    // get recent trc20 txs
     const txs = await db.getTransactionsTrc20();
     const receivers = txs.map((item)=>{
         return item.to;
     });
 
-    // reduce to distinct addresses
-    const distinct_addresses = [...new Set(receivers)];
+    console.log('receivers: ', receivers.length);
 
-    // get trc20 configs
+    const receivers_filt = receivers.filter((item)=>{
+        if (item!=poolAddr) return true;
+    });
+
+    console.log('poolAddr removed', receivers_filt.length);
+    
+    const distinct_addresses = [...new Set(receivers_filt)];
+    console.log('unique ', receivers_filt.length);
+
     const conf = require('../configs/token.config.json')[token];
 
-    // iterate distinct addresses
     for (let addr of distinct_addresses) {
+        const balance_str = await getUsdt(addr, conf.contract, conf.digit);
+        const balance_float = parseFloat(balance_str);
+        console.log(`found token: ${balance_str}`);
+        await delay(1000);
 
-        const balance_str = await tron.getBalanceTrc20(addr, conf.contract, conf.digit);
-        const balance_token = parseFloat(balance_str);
-        
-        console.log(`found token: ${balance_str}`)
+        if (balance_float<conf.min_balance) continue; 
 
-        if (balance_token<conf.min_balance) continue; // if no trc20 balance, continue
+        const balance_trx_str = await getNative(addr);
+        const balance_trx_float = parseFloat(balance_trx_str);
 
-        let balance_trx = await tron.getBalanceNative(addr);
-        balance_trx = parseFloat(balance_trx);
-        
-        if (balance_trx<parseFloat(process.env.TRXFORGAS)) { // if trx balance < 7.5
-            // replenish trx for gas
+        if (balance_trx<parseFloat(process.env.TRXFORGAS)) { 
             const amount_raw = parseFloat(process.env.TRXFORGAS) * 10**6;
             const encrypted = process.env.SECRET;
             const decrypted = encryption.decryptKey(encrypted);
 
-            const txid = await tron.sendNative(poolAddr, addr, amount_raw, decrypted);
+            console.log('replenish gas to: ', addr);
+            const txid = await sendTrx(poolAddr, addr, amount_raw, decrypted);
 
             if (!txid || txid.length!=64 ){
-                throw new Error(`replenix trx failed`);
+                throw new Error(`replenish trx failed`);
             }
             console.log(`successful native tx: ${txid}`);
-        } else { // else 
+        } else {
             // forward to pool
-            const balance_raw = balance_token *10**conf.digit;
-            const encrypted = await db.getPrivate(addr);
+            const balance_raw = balance_float *10**conf.digit;
+            const encrypted = await getPk(addr);
             const decrypted = encryption.decryptKey(encrypted);
 
-            const txid = await tron.sendToken(addr, poolAddr, balance_raw, conf.contract, decrypted);
+            const txid = await tron.sendUsdt(addr, poolAddr, balance_raw, conf.contract, decrypted);
 
             if (!txid || txid.length!=64 ){
                 throw new Error(`forwarding ${token} failed`);
             }
             console.log(`successful token tx: ${txid}`);
         }
-    } // end for distinct_addresses
-    console.log('forwarding session ended');
+        console.log('forwarding session ended');
+    }
 }
 
 module.exports = {
